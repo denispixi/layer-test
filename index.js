6278dc171a42a61991f8fc982f8a77411e7c82d6
Denis Adhemar <denispixi@gmail.com>
2021-12-23 19:11:27 -0500

    updated sum method

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const greeting = name => console.log(`Hello ${name}`)
-const sumNumbers = (...args) => args.reduce((a, b) => a + b, 0);
+const sumNumbers = (...args) => {
+  const sum = args.reduce((a, b) => a + b, 0)
+  console.log(`The sum is ${sum}`);
+}
 
 module.exports = {
   greeting,
